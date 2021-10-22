bef57a2164d2617001d3d1f13687c716f663f44a
devmccraney <devmccraney@gmail.com>
2021-10-22 18:23:10 -0400

    added animations

---
+++ js/index.js
@@ -0,0 +1,38 @@
+$(".lmtr").hover(function(){
+    $(".lmtr").animate({
+        width: '450px',
+        height: '420px'
+    }, 400);
+},
+function(){
+    $(".lmtr").animate({
+        width: '400px',
+        height: '350px'
+    },400);
+});
+
+$(".tb").hover(function(){
+    $(".tb").animate({
+        width: '450px',
+        height: '380px'
+    }, 400);
+},
+function(){
+    $(".tb").animate({
+        width: '400px',
+        height: '320px'
+    },400);
+});
+
+$(".mt").hover(function(){
+    $(".mt").animate({
+        width: '450px',
+        height: '400px'
+    }, 400);
+},
+function(){
+    $(".mt").animate({
+        width: '400px',
+        height: '370px'
+    },400);
+});
